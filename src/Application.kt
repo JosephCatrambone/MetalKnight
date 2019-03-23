1758fd860d5787edcc7d949548e02deb1ed2b95e
Joseph Catrambone <jo.jcat@gmail.com>
2019-03-22 19:46:29 -0700

    Initial import.

---
+++ src/Application.kt
@@ -0,0 +1,129 @@
+package io.xoana
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import freemarker.cache.*
+import io.ktor.freemarker.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.webjars.*
+import java.time.*
+import io.ktor.auth.*
+import io.ktor.client.features.auth.basic.*
+import io.ktor.client.*
+import io.ktor.client.engine.jetty.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(Webjars) {
+        path = "/webjars" //defaults to /webjars
+        zone = ZoneId.systemDefault() //defaults to ZoneId.systemDefault()
+    }
+
+    // https://ktor.io/servers/features/https-redirect.html#testing
+    if (!testing) {
+        install(HttpsRedirect) {
+            // The port to redirect to. By default 443, the default HTTPS port.
+            sslPort = 443
+            // 301 Moved Permanently, or 302 Found redirect.
+            permanentRedirect = true
+        }
+    }
+
+    install(Authentication) {
+        basic("myBasicAuth") {
+            realm = "Ktor Server"
+            validate { if (it.name == "test" && it.password == "password") UserIdPrincipal(it.name) else null }
+        }
+    }
+
+    install(ContentNegotiation) {
+    }
+
+    val client = HttpClient(Jetty) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-freemarker") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("data" to IndexData(listOf(1, 2, 3))), ""))
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        get("/webjars") {
+            call.respondText("<script src='/webjars/jquery/jquery.js'></script>", ContentType.Text.Html)
+        }
+
+        authenticate("myBasicAuth") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+    }
+}
+
+data class IndexData(val items: List<Int>)
+
+data class MySession(val count: Int = 0)
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
---
+++ test/ApplicationTest.kt
@@ -0,0 +1,34 @@
+package io.xoana
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import freemarker.cache.*
+import io.ktor.freemarker.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.sessions.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.webjars.*
+import java.time.*
+import io.ktor.auth.*
+import io.ktor.client.features.auth.basic.*
+import io.ktor.client.*
+import io.ktor.client.engine.jetty.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
